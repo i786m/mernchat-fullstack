c73291f0b319a9e64d612076936b8b7ba5681ad4
Imran Mohamed <ibm786@icloud.com>
2021-06-05 23:39:29 +0100

    modified axios and rebuilt app

--- front-end/src/axios.js
+++ front-end/src/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
   // baseURL: 'http://localhost:2121',
-  baseURL: 'https://mernchat-i786m.herokuapp.com/',
+  baseURL: 'https://mernchat-i786m.herokuapp.com',
 });
 
 export default instance;
